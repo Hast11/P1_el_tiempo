44e24dc04f2602e00aab2ff7a18466211b80f6a4
Hast11 <andres2000gil@gmail.com>
2023-11-15 14:40:14 +0100

    import useState

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Resultados from './Resultados';
 import Header from './Header';
-import React from 'react';
+import {useState} from 'react';
 
 function App() {
   return (
