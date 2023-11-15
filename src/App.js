985f654b86c3ddd6562f758aaaccc1d95d9c4eeb
Hast11 <andres2000gil@gmail.com>
2023-11-15 14:44:18 +0100

    import config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Resultados from './Resultados';
 import Header from './Header';
 import {useState} from 'react';
+import CONFIG from './config/config';
+
+const USE_SERVER = CONFIG.use_server;
 
 function App() {
   return (
