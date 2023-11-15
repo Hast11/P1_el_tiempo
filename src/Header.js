17727198b3b0881055f952f98e7fd53afaa5c12f
Hast11 <andres2000gil@gmail.com>
2023-11-15 14:49:19 +0100

    Header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
-export default function Header(props){
-    return
-        <div>
-            
+import './Header.css'
+export default function Header (props){
+    return( //Todos los components tienen que tener un return que devuelva un elemento unico
+        <div id="cabecera">
+            <link href="Header.css" rel="stylesheet"></link>
+            <h3 id="titulo" className='mensaje'>Pagina de Andres Gil Oliva</h3> {/* CSS para centrarlo */}
+            <center><img className="logo" src="29264.png" width={50} alt="Logo"></img></center>
         </div>
+    )
 }
