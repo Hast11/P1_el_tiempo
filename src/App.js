df45d9434dbbb101afdc023a8265465eb4b318b8
Hast11 <andres2000gil@gmail.com>
2023-11-15 14:52:24 +0100

    CONSTANTES

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import {useState} from 'react';
 import CONFIG from './config/config';
 
 const USE_SERVER = CONFIG.use_server;
+const url = CONFIG.server_url;
+const defaultLat = CONFIG.default_lat
+const defaultLon = CONFIG.default_lon;
+const numItems = CONFIG.num_items;
+
 
 function App() {
   return (
