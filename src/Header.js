be5032e7a516cf67d02031f41187e48f014a37f8
Hast11 <andres2000gil@gmail.com>
2023-11-15 15:25:06 +0100

    quitar id

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ export default function Header (props){
     return( //Todos los components tienen que tener un return que devuelva un elemento unico
         <div id="cabecera">
             <link href="Header.css" rel="stylesheet"></link>
-            <h3 id="titulo" className='mensaje'>Pagina de Andres Gil Oliva</h3> {/* CSS para centrarlo */}
+            <h3 className='mensaje'>Pagina de Andres Gil Oliva</h3> {/* CSS para centrarlo */}
             <center><img className="logo" src="29264.png" width={50} alt="Logo"></img></center>
         </div>
     )
