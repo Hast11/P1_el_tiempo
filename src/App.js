a8a3ad8461ce437ca4ff415bcd1135a284fa97c5
Hast11 <andres2000gil@gmail.com>
2023-11-15 17:35:40 +0100

    llamada API

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Resultados from './Resultados';
 import Header from './Header';
 import {useState} from 'react';
 import CONFIG from './config/config';
-import { mock1 } from './constants/users';
+import { mock1 } from './constants/mock';
+import Error from './Error';
 
 const USE_SERVER = CONFIG.use_server;
 const url = CONFIG.server_url;
 const defaultLat = CONFIG.default_lat
 const defaultLon = CONFIG.default_lon;
 const numItems = CONFIG.num_items;
+var aux = true;
 
 
 function App() {
@@ -21,7 +23,27 @@ function App() {
 
   const callServer = async (param) => {
     if(USE_SERVER){
-
+      try{
+        let queryparams ="";
+        queryparams ="?lat=" + latitud + "&lon=" + longitud + "&appid=" + CONFIG.api_key;
+        const response = await fetch(`${url}${queryparams}`);
+        if (Response.status===200){
+          aux = true;
+          const data = await response.json(); 
+          setResultado(data);
+        }
+        else{
+          aux = false;
+          const data = await response.json(); 
+          setResultado(data);
+          <Error/>
+        }
+      }
+      catch(error){
+        <div>
+          Error {setResultado({error: {description: error.message}})}
+        </div>
+      }
     }
     else{
       setResultado(mock1);
@@ -35,7 +57,8 @@ function App() {
       <div>Latitud:<input id="latitud" type="number" placeholder={latitud} value={latitud} onChange={e=>setLatitud(e.target.value)}/></div> {/* Importante que el value es el estado, no el default */}
       <div>Longitud:<input id="longitud" type="number" placeholder={longitud} value={longitud} onChange={e=>setLongitud(e.target.value)}/></div>
       <div><button id="buscar">Buscar</button></div>
-      {/* <Resultados numItems={numItems} items={null} /> */}
+      {(resultado && aux) && <Resultados numitems={CONFIG.num_items} items={resultado}/>}
+      {!aux && <Error error={resultado}/>}
     </div>
   );
 }
